83598be7d3cbb524b43728a3544ed716b73655c3
Marissa Montgomery <m@m15y.com>
2024-02-17 11:46:09 -0500

    Add execute flag for commit.

--- namecommit.zsh
+++ namecommit.zsh
@@ -1,8 +1,14 @@
 commitm() {
     local system_prompt='Based on these changes, suggest a concise commit message, ideally less than 5 words:'
+    local execute_commit=false
     local git_output_temp_file=$(mktemp)
     local commit_message_temp_file=$(mktemp)
     local cleaned_up=false # Flag to indicate whether cleanup has been run
+    
+    # Check for the execute flag (-e)
+    if [[ "$1" == "--execute" ]] || [[ "$1" == "-e" ]]; then
+        execute_commit=true
+    fi
 
     cleanup() {
         # Only run cleanup if it hasn't been done yet
@@ -47,12 +53,18 @@ commitm() {
 
     # Output the commit message and copy command to clipboard
     local commit_message=$(cat "$commit_message_temp_file")
-    echo "Suggested commit message: $commit_message"
-    local commit_command="git commit -m '$commit_message'"
-    echo "$commit_command" | pbcopy
+    echo -e "Generated commit message: \e[1m\e[34m$commit_message\e[0m\n"
+    
+    # Execute the git commit command with the generated message if the execute flag is set
+    if [[ "$execute_commit" == true ]]; then
+        git commit -m "$commit_message"
+    else
+        local commit_command="git commit -m '$commit_message'"
+        echo "$commit_command" | pbcopy
+        echo "The commit command has been copied to your clipboard. You can paste it into your terminal to commit."
 
-    echo "The commit command has been copied to your clipboard. You can paste it into your terminal to commit."
-
+    fi
+    
     # Perform cleanup
     cleanup
 }
